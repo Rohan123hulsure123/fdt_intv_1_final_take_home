16ccb735cf3ad885069d64943de308f0012d9039
Rohan123hulsure123 <rohan.hulsure@gmail.com>
2023-12-30 01:14:54 +0530

    About US Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import AboutUs from './components/AboutUs/AboutUs';
 import HomeSection from './components/HomeSection/HomeSection';
 
 function App() {
   return (
     <div className="App">
       <HomeSection />
+      <AboutUs />
     </div>
   );
 }
