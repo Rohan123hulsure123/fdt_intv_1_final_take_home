2b6a1c80510b7ee203944731eb803681ca34918d
Rohan123hulsure123 <rohan.hulsure@gmail.com>
2023-12-30 02:17:42 +0530

    #Added, Why choose us

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import HomeSection from './components/HomeSection/HomeSection';
+import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs';
 
 function App() {
   return (
     <div className="App">
       <HomeSection />
       <AboutUs />
+      <WhyChooseUs />
     </div>
   );
 }
